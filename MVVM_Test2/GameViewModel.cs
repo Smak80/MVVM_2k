eac1c7db8510a6cd6ebebe0ca47643263437af19
Sergey Makletsov <smaklets@kpfu.ru>
2024-03-09 13:35:34 +0300

    Удаление тестовых элементов, которые не нужны

--- MVVM_Test2/GameViewModel.cs
+++ MVVM_Test2/GameViewModel.cs
@@ -2,24 +2,13 @@
 
 namespace MVVM_Test2;
 
-public class GameViewModel
+public class GameViewModel(User player)
 {
-    private readonly User _player;
-    private readonly List<Test> _t = new();
-    public List<Test> T => _t.ToList();
-
     public User Player
     {
-        get => _player.Clone(); 
-        init => _player = value;
+        get => player.Clone(); 
+        init => player = value;
     }
 
     public Field Field { get; } = new(30, 17);
-
-    public GameViewModel(User player)
-    {
-        _player = player;
-        _t.Add(new Test {Left = 30, Top = 10, WhSize = 100});
-        _t.Add(new Test { Left = 130, Top = 20, WhSize = 100 });
-    }
 }
